0a0da40b0a2c99759ae4d542ced88035274330b4
Gonzalo García <gonzalo@neoland.es>
2020-11-20 17:02:02 +0100

    * related model on querys

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,16 +1,57 @@
 import { log } from 'console';
 import { Request, Response } from 'express';
+import { Op } from 'sequelize';
 import { User } from '../models/user.model';
+import { Product } from '../models/product.model';
+import { Provider } from '../models/provider.model';
 
 class UsersController {
 
-    public index (req: Request, res: Response) {
-        res.send('INDEX, la ruta funciona');
+    public async index (req: Request, res: Response) {        
+    
+        try{
+            // SELECT * FROM USERS WHERE name = 'Antonio' AND id:3 AND familyName = 'Lozano' OR familyName='Belén'
+            const users = await User.findAll({ 
+                where: {
+                    name: {
+                        [Op.like] : '%A%'
+                    },
+                    id: 3,
+                    [Op.or] : [
+                        {familyName: 'Lozano'},
+                        {familyName: 'Belén'}
+                    ]
+                },
+                raw: true
+            });
+            const userAntonio = await User.findByPk(3, { raw: true});
+
+            if(userAntonio && users){
+                res.send(users);
+            }else{
+                res.sendStatus(404);
+            }
+            
+        } catch (error) {
+            console.log(error);
+            res.sendStatus(500);
+        }   
     }
 
-    public show (req: Request, res: Response) {
+    public async show (req: Request, res: Response) {
 
-        res.send('SHOW, la ruta funciona');
+        const product: Product[] = await Product.findAll(
+            {
+                include: [
+                    {
+                        model: Provider
+                    }
+                ]   
+            }
+        );
+
+        res.send(product);
+        
     }
 }
 
