0702a4a4172f41e61a3799736ca0146853f34f64
Gonzalo García <gonzalo@neoland.es>
2020-11-23 11:27:21 +0100

    * crud

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,8 @@ app.use('/users', userRoutes.router);
 
 // Start the server, using the port defined
 app.listen(app.get('port'), () => {
+
     console.log(`Ther server is running on port ${app.get('port')}`); 
+    
 });
 
